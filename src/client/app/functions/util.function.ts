a79ddbb7aa83b409bc0a99c01d4139a49f6dbdb3
hataguchi <hataguchi@motionpicture.jp>
2021-03-09 18:15:59 +0900

    ### Changed  - APIページング待ち時間変更 (500ms => 100ms)

--- src/client/app/functions/util.function.ts
+++ src/client/app/functions/util.function.ts
@@ -74,7 +74,7 @@ export async function retry<T>(args: {
  * ミリ秒待ち
  * @param time
  */
-export async function sleep(time: number = 500) {
+export async function sleep(time: number = 100) {
     return new Promise<void>((resolve) => {
         setTimeout(() => {
             resolve();

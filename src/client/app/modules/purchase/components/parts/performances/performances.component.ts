f340fed4c3ddb44f514cc2ea4f64ea8738b1935d
hataguchi <hataguchi@motionpicture.jp>
2021-09-01 15:03:29 +0900

    ### Changed  - デザイン変更

--- src/client/app/modules/purchase/components/parts/performances/performances.component.ts
+++ src/client/app/modules/purchase/components/parts/performances/performances.component.ts
@@ -1,30 +1,35 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { factory } from '@cinerino/sdk';
 import * as moment from 'moment';
-import { Functions } from '../../../../..';
+import { Functions, Models } from '../../../../..';
+import { getEnvironment } from '../../../../../../environments/environment';
 
 @Component({
     selector: 'app-purchase-performances',
     templateUrl: './performances.component.html',
-    styleUrls: ['./performances.component.scss']
+    styleUrls: ['./performances.component.scss'],
 })
 export class PurchasePerformancesComponent implements OnInit {
-
-    @Input() public screeningEventsGroup: Functions.Purchase.IScreeningEventsGroup;
-    @Input() public screeningEventSeries: factory.chevre.event.screeningEventSeries.IEvent;
-    @Input() public videoFormatTypes: factory.chevre.categoryCode.ICategoryCode[];
+    @Input()
+    public screeningEventsGroup: Functions.Purchase.IScreeningEventsGroup;
+    @Input()
+    public screeningEventSeries: factory.chevre.event.screeningEventSeries.IEvent;
+    @Input()
+    public videoFormatTypes: factory.chevre.categoryCode.ICategoryCode[];
     @Input() public animation?: boolean;
-    @Output() public select = new EventEmitter<factory.chevre.event.screeningEvent.IEvent>();
+    @Output() public select =
+        new EventEmitter<factory.chevre.event.screeningEvent.IEvent>();
     public moment = moment;
+    public viewType = Models.Util.ViewType;
+    public environment = getEnvironment();
 
-    constructor() { }
+    constructor() {}
 
     public ngOnInit() {
-        this.animation = (this.animation === undefined) ? false : this.animation;
+        this.animation = this.animation === undefined ? false : this.animation;
     }
 
     public getVideoFormatType(code: string) {
-        return this.videoFormatTypes.find(v => v.codeValue === code);
+        return this.videoFormatTypes.find((v) => v.codeValue === code);
     }
-
 }

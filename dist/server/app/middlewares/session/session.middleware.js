d1840e77e3c89334d0613e08b994c930086162b8
hataguchi <hataguchi@motionpicture.jp>
2021-12-16 18:38:29 +0900

    ### Changed - log出力をwinstonへ変更

--- dist/server/app/middlewares/session/session.middleware.js
+++ dist/server/app/middlewares/session/session.middleware.js
@@ -12,13 +12,13 @@ const redisClient = redis.createClient({
     port: Number(process.env.REDIS_PORT),
     host: process.env.REDIS_HOST,
     password: process.env.REDIS_KEY,
-    tls: (process.env.REDIS_TLS_SERVERNAME === undefined
-        || process.env.REDIS_TLS_SERVERNAME === '')
+    tls: process.env.REDIS_TLS_SERVERNAME === undefined ||
+        process.env.REDIS_TLS_SERVERNAME === ''
         ? undefined
         : {
-            servername: process.env.REDIS_TLS_SERVERNAME
+            servername: process.env.REDIS_TLS_SERVERNAME,
         },
-    return_buffers: true
+    return_buffers: true,
 });
 debug('redis host...', process.env.REDIS_HOST);
 const sessionStore = new (connectRedis(session))({ client: redisClient });
@@ -31,8 +31,9 @@ exports.default = session({
     cookie: {
         secure: true,
         httpOnly: true,
-        maxAge: (process.env.SESSION_COOKIE_MAX_AGE === undefined
-            || process.env.SESSION_COOKIE_MAX_AGE === '')
-            ? 604800000 : Number(process.env.SESSION_COOKIE_MAX_AGE)
-    }
+        maxAge: process.env.SESSION_COOKIE_MAX_AGE === undefined ||
+            process.env.SESSION_COOKIE_MAX_AGE === ''
+            ? 604800000
+            : Number(process.env.SESSION_COOKIE_MAX_AGE),
+    },
 });

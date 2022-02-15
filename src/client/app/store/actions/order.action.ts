d1360c34a972c2ed7a77a71ce8a2edc8453a119e
hataguchi <hataguchi@motionpicture.jp>
2022-02-15 15:16:04 +0900

    ### Added - storeServiceを追加

--- src/client/app/store/actions/order.action.ts
+++ src/client/app/store/actions/order.action.ts
@@ -1,6 +1,5 @@
 import { factory } from '@cinerino/sdk';
 import { createAction, props } from '@ngrx/store';
-import { Models } from '../..';
 const LABEL = '[Order]';
 
 export const remove = createAction(`${LABEL} remove`);
@@ -10,60 +9,4 @@ export const setOrder = createAction(
     props<{
         order: factory.order.IOrder;
     }>()
-);
-
-export const cancel = createAction(
-    `${LABEL} cancel`,
-    props<{
-        orders: factory.order.IOrder[];
-        language: string;
-        agent?: {
-            identifier?: factory.person.IIdentifier;
-        };
-    }>()
-);
-
-export const cancelSuccess = createAction(`${LABEL} cancelSuccess`);
-
-export const cancelFail = createAction(
-    `${LABEL} cancelFail`,
-    props<{ error: Error }>()
-);
-
-export const inquiry = createAction(
-    `${LABEL} inquiry`,
-    props<{
-        confirmationNumber: string;
-        customer: {
-            email?: string;
-            telephone?: string;
-        };
-    }>()
-);
-
-export const inquirySuccess = createAction(
-    `${LABEL} inquirySuccess`,
-    props<{ order: factory.order.IOrder }>()
-);
-
-export const inquiryFail = createAction(
-    `${LABEL} inquiryFail`,
-    props<{ error: Error }>()
-);
-
-export const print = createAction(
-    `${LABEL} print`,
-    props<{
-        orders: factory.order.IOrder[];
-        printer: Models.Util.Printer.IPrinter;
-        pos?: factory.chevre.place.movieTheater.IPOS;
-        timeout?: number;
-    }>()
-);
-
-export const printSuccess = createAction(`${LABEL} printSuccess`);
-
-export const printFail = createAction(
-    `${LABEL} printFail`,
-    props<{ error: Error }>()
 );
